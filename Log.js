0f278f6a638d3794c2011950df07fb128af740f7
yashvijayan <82642704+yashvijayan@users.noreply.github.com>
2021-07-21 19:08:47 +0530

    Add files via upload

---
+++ Log.js
@@ -0,0 +1,10 @@
+class Log extends Baseclass  {
+    constructor(x, y, height, angle) {
+     super(x,y,20,height);
+      this.image=loadImage("sprites/wood2.png");
+      Matter.Body.setAngle(this.body, angle);
+      
+    }
+    
+  };
+  
